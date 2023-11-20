4acf4c0be5fa8c810a1b688040115326cf60106e
Agam More <agam@agam.me>
2023-11-20 11:53:35 -0600

    Add a few vite

--- vite.settings.config.ts
+++ vite.settings.config.ts
@@ -4,5 +4,11 @@ import path from "path";
 
 // https://vitejs.dev/config
 export default defineConfig({
+  build: {
+    outDir: ".vite/build/",
+    rollupOptions: {
+      external: ["better-sqlite3"],
+    },
+  },
   root: path.join(__dirname, "src", "settings"),
 });
